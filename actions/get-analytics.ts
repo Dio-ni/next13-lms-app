f0fe54d072a50ebcc22c40b90841324f22fde219
Dio-ni <200103264@stu.sdu.edu.kz>
2025-04-01 18:17:11 +0500

    no purchcase atgetanalytics

--- actions/get-analytics.ts
+++ actions/get-analytics.ts
@@ -1,59 +1,32 @@
 import { db } from '@/lib/db';
-import { Course, Purchase } from '@prisma/client';
-
-type PurchaseWithCourse = Purchase & {
-  course: Course;
-};
-
-const groupByCourse = (purchases: PurchaseWithCourse[]) => {
-  const grouped: { [courseTitle: string]: number } = {};
-
-  purchases.forEach((purchase) => {
-    const courseTitle = purchase.course.title;
-    if (!grouped[courseTitle]) {
-      grouped[courseTitle] = 0;
-    }
-    grouped[courseTitle] += purchase.course.price!;
-  });
-
-  return grouped;
-};
+import { Course } from '@prisma/client';
 
 export const getAnalytics = async (userId: string) => {
   try {
-    const purchases = await db.purchase.findMany({
+    const courses = await db.course.findMany({
       where: {
-        course: {
-          userId: userId,
-        },
+        userId: userId,
       },
-      include: {
-        course: true,
+      select: {
+        title: true,
+        price: true,
       },
     });
 
-    const groupedEarnings = groupByCourse(purchases);
-    const data = Object.entries(groupedEarnings).map(
-      ([courseTitle, total]) => ({
-        name: courseTitle,
-        total: total,
-      })
-    );
-
-    const totalRevenue = data.reduce((acc, curr) => acc + curr.total, 0);
-    const totalSales = purchases.length;
+    const totalRevenue = courses.reduce((acc, course) => acc + (course.price || 0), 0);
 
     return {
-      data,
+      data: courses.map((course) => ({
+        name: course.title,
+        total: course.price || 0,
+      })),
       totalRevenue,
-      totalSales,
     };
   } catch (error) {
     console.log('[GET_ANALYTICS]', error);
     return {
       data: [],
       totalRevenue: 0,
-      totalSales: 0,
     };
   }
 };
